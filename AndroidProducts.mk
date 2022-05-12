df0d2ab60938cb686e3d590262ce35fbd806b23f
Sajith Madhusankha <85398592+Sajith-Madhusankha@users.noreply.github.com>
2022-05-12 12:20:07 +0530

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/awaken_juice.mk
+
+COMMON_LUNCH_CHOICES := \
+    awaken_juice-user \
+    awaken_juice-userdebug \
+    awaken_juice-eng
---
+++ awaken_juice.mk
@@ -0,0 +1,27 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/juice/device.mk)
+
+# Inherit some common awaken stuff.
+$(call inherit-product, vendor/awaken/config/common.mk)
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+TARGET_GAPPS_ARCH := arm64
+
+# Bootanimation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2340
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := awaken_juice
+PRODUCT_DEVICE := juice
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := sm6115
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.211001.001/7641976:user/release-keys"
